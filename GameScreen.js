55d0d45e10967991be73b350f75fe5ec2ea42aee
Aditya Vinayak <60056272+itsaditya98@users.noreply.github.com>
2023-10-18 17:20:00 +0530

    Add files via upload

--- GameScreen.js
+++ GameScreen.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { WebView } from 'react-native-webview';
 
 const GameScreen = () => {
-  return <WebView source={{ uri: 'C:\Users\adity\OneDrive\Documents\GAME2\gamep.html' }} />;
+  return <WebView source={{ uri: 'C:\Users\adity\OneDrive\Documents\GAME2\index.html' }} />;
 };
 
 export default GameScreen;
